79a6c6f429dfd7f03414c17adbc6e3ccdd27b55f
Arno Firdaus <ruaz.anfield@gmail.com>
2024-11-28 21:13:28 +0700

    feat: hello world

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack } from "@chakra-ui/react"
+import { Box, HStack, Text } from "@chakra-ui/react"
 import { RiArrowRightLine, RiMailLine } from "react-icons/ri"
 
 import { Button } from "@/components/ui/button"
@@ -6,6 +6,8 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <Box padding="10" rounded="sm" borderWidth="1px">
+      <Text fontSize="5xl">Hello World</Text>
+
       <HStack>
         <Button colorPalette="teal" variant="solid">
           <RiMailLine /> Email
